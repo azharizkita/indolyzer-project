59a1bdf5b47dd96a25a7e78803de5f2d13b7bd17
Azhari Rizkita Priyono <azharizkita@outlook.com>
2018-10-09 02:05:24 +0700

    Major animation update

--- js/init.js
+++ js/init.js
@@ -6,11 +6,61 @@
     $('.parallax').parallax();
     $('.scrollspy').scrollSpy({
       scrollOffset: 60,
-      getActiveElement:   function(id) {
-        console.log(id)
+      getActiveElement: function(id) {
+        if (id == "layanan") {
+          $("#moto").css("animation-play-state", "running");
+          setTimeout(function() { 
+            $("#iotCard").css("animation-play-state", "running");
+            setTimeout(function() { 
+              $("#itcCard").css("animation-play-state", "running");
+              setTimeout(function() { 
+                $("#adCard").css("animation-play-state", "running");
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }
+
+        if (id == "about") {
+          $("#aboutText").css("animation-play-state", "running");
+        }
+
+        if (id == "penanggungjawab") {
+          $("#moto2").css("animation-play-state", "running");
+          setTimeout(function() { 
+            $("#manCard").css("animation-play-state", "running");
+            setTimeout(function() { 
+              $("#ceo1").css("animation-play-state", "running");
+              setTimeout(function() { 
+                $("#ceo2").css("animation-play-state", "running");
+                $("#ceo3").css("animation-play-state", "running");
+                setTimeout(function() { 
+                  $("#se1").css("animation-play-state", "running");
+                  $("#he1").css("animation-play-state", "running");
+                  setTimeout(function() { 
+                    $("#se2").css("animation-play-state", "running");
+                    $("#he2").css("animation-play-state", "running");
+                    $("#se3").css("animation-play-state", "running");
+                    $("#he3").css("animation-play-state", "running");
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }
+
+        if (id == "contact") {
+          $("#contactInfo").css("animation-play-state", "running");
+          setTimeout(function() { 
+            $("#blockDown").css("animation-play-state", "running");
+            setTimeout(function() { 
+              $("#downBeat").css("animation-play-state", "running");
+            }, 1000);
+          }, 1000);
+        }
+
       }
     });
     $('.tabs').tabs();
 
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+  });
+})(jQuery);
